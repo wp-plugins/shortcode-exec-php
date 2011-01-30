9a0c1106a3098e3f95854393462164c502b94ad7
M66B <M66B@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-30 22:44:13 +0000

    1.19 description
    git-svn-id: https://plugins.svn.wordpress.org/shortcode-exec-php/trunk@338959 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shortcode-exec-php.php
+++ shortcode-exec-php.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Shortcode Exec PHP
 Plugin URI: http://blog.bokhorst.biz/3626/computers-en-internet/wordpress-plugin-shortcode-exec-php/
-Description: Execute reusable PHP code in posts, pages and widgets using shortcodes
+Description: Execute arbitrary, reusable PHP code in posts, pages, comments, widgets and RSS feeds using shortcodes in a safe and easy way
 Version: 1.19
 Author: Marcel Bokhorst
 Author URI: http://blog.bokhorst.biz/about/
