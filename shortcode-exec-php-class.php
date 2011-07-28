1d4ea8290d1722eff5ce6c19987316067ce2c764
M66B <M66B@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-28 10:55:34 +0000

    1.33 release
    git-svn-id: https://plugins.svn.wordpress.org/shortcode-exec-php/tags/1.33@416326 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shortcode-exec-php-class.php
+++ shortcode-exec-php-class.php
@@ -17,6 +17,7 @@ define('c_scep_option_rss', 'scep_rss');
 define('c_scep_option_noent', 'scep_noent');
 define('c_scep_option_cleanup', 'scep_cleanup');
 define('c_scep_option_donated', 'scep_donated');
+define('c_scep_option_nospsn', 'scep_nospsn');
 define('c_scep_option_codewidth', 'scep_codewidth');
 define('c_scep_option_codeheight', 'scep_codeheight');
 define('c_scep_option_backtrack_limit', 'scep_backtrack_limit');
@@ -154,8 +155,7 @@ if (!class_exists('WPShortcodeExecPHP')) {
 
 				// Enqueue scripts
 				wp_enqueue_script('jquery');
-				$plugin_dir = '/' . PLUGINDIR .  '/' . basename(dirname($this->main_file));
-				wp_enqueue_script('editarea', $plugin_dir . '/editarea/edit_area/edit_area_full.js');
+				wp_enqueue_script('editarea', $this->plugin_url . '/editarea/edit_area/edit_area_full.js');
 
 				// Enqueue style sheet
 				$css_name = $this->Change_extension(basename($this->main_file), '.css');
@@ -238,6 +238,7 @@ if (!class_exists('WPShortcodeExecPHP')) {
 				WPShortcodeExecPHP::Delete_option(c_scep_option_recursion_limit);
 				WPShortcodeExecPHP::Delete_option(c_scep_option_cleanup);
 				WPShortcodeExecPHP::Delete_option(c_scep_option_donated);
+				WPShortcodeExecPHP::Delete_option(c_scep_option_nospsn);
 
 				$name = WPShortcodeExecPHP::Get_option(c_scep_option_names);
 				for ($i = 0; $i < count($name); $i++) {
@@ -351,6 +352,8 @@ if (!class_exists('WPShortcodeExecPHP')) {
 					$_POST[c_scep_option_cleanup] = null;
 				if (empty($_POST[c_scep_option_donated]))
 					$_POST[c_scep_option_donated] = null;
+				if (empty($_POST[c_scep_option_nospsn]))
+					$_POST[c_scep_option_nospsn] = null;
 
 				// Update settings
 				if (WPShortcodeExecPHP::Is_multisite() && function_exists('update_site_option'))
@@ -370,6 +373,7 @@ if (!class_exists('WPShortcodeExecPHP')) {
 				WPShortcodeExecPHP::Update_option(c_scep_option_author_cap, $_POST[c_scep_option_author_cap]);
 				WPShortcodeExecPHP::Update_option(c_scep_option_cleanup, $_POST[c_scep_option_cleanup]);
 				WPShortcodeExecPHP::Update_option(c_scep_option_donated, $_POST[c_scep_option_donated]);
+				WPShortcodeExecPHP::Update_option(c_scep_option_nospsn, $_POST[c_scep_option_nospsn]);
 
 				$this->Configure_prce();
 
@@ -441,6 +445,7 @@ if (!class_exists('WPShortcodeExecPHP')) {
 			$scep_option_author_cap = WPShortcodeExecPHP::Get_option(c_scep_option_author_cap);
 			$scep_cleanup = (WPShortcodeExecPHP::Get_option(c_scep_option_cleanup) ? 'checked="checked"' : '');
 			$scep_donated = (WPShortcodeExecPHP::Get_option(c_scep_option_donated) ? 'checked="checked"' : '');
+			$scep_nospsn = (WPShortcodeExecPHP::Get_option(c_scep_option_nospsn) ? 'checked="checked"' : '');
 
 			// Default size
 			if ($scep_width <= 0)
@@ -581,6 +586,12 @@ if (!class_exists('WPShortcodeExecPHP')) {
 				<label for="scep_option_donated"><?php _e('I have donated to this plugin', c_scep_text_domain); ?></label>
 			</th><td>
 				<input id="scep_option_donated" name="<?php echo c_scep_option_donated; ?>" type="checkbox"<?php echo $scep_donated; ?> />
+			</td></tr>
+
+			<tr valign="top"><th scope="row">
+				<label for="scep_option_nospsn"><?php _e('I don\'t want to support this plugin with the Sustainable Plugins Sponsorship Network', c_scep_text_domain); ?></label>
+			</th><td>
+				<input id="scep_option_nospsn" name="<?php echo c_scep_option_nospsn; ?>" type="checkbox"<?php echo $scep_nospsn; ?> />
 			</td></tr>
 
 			</table>
@@ -824,7 +835,7 @@ if (!class_exists('WPShortcodeExecPHP')) {
 		}
 
 		function Render_pluginsponsor() {
-			if (!WPShortcodeExecPHP::Get_option(c_scep_option_donated)) {
+			if (!WPShortcodeExecPHP::Get_option(c_scep_option_nospsn)) {
 ?>
 				<script type="text/javascript">
 				var psHost = (("https:" == document.location.protocol) ? "https://" : "http://");
@@ -911,6 +922,15 @@ if (!class_exists('WPShortcodeExecPHP')) {
 
 				// Load text domain
 				load_plugin_textdomain(c_scep_text_domain, false, basename(dirname($this->main_file)));
+
+				if (empty($_REQUEST[c_scep_param_name]))
+					$_REQUEST[c_scep_param_name] = null;
+				if (empty($_REQUEST[c_scep_param_shortcode]))
+					$_REQUEST[c_scep_param_shortcode] = null;
+				if (empty($_REQUEST[c_scep_param_description]))
+					$_REQUEST[c_scep_param_description] = null;
+				if (empty($_REQUEST[c_scep_param_phpcode]))
+					$_REQUEST[c_scep_param_phpcode] = null;
 
 				if (empty($_REQUEST[c_scep_param_enabled]))
 					$_REQUEST[c_scep_param_enabled] = false;
